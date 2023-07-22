764e9a3c7967c803feea527757ab7f0fdb722c9b
abdurrahmanekecik <abdurrahmanekecik@gmail.com>
2023-07-22 10:27:12 +0300

    ChatController & Route update

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chatmessage;
+use App\Models\Chatroom;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ChatController extends Controller
+{
+    public function rooms(Request $request){
+        return Chatroom::all();
+    }
+
+    public function messages(Request $request, $roomId){
+        return Chatmessage::where("chat_room_id", $roomId)
+            ->with('user')
+            ->orderBy('create_at','DESC')
+            ->get();
+    }
+    public function newMessage(Request $request, $roomId){
+        $newMessage = new Chatmessage();
+        $newMessage->user_id = Auth::id();
+        $newMessage->chat_room_id = $roomId;
+        $newMessage->message = $request->message;
+        $newMessage->save();
+
+        return $newMessage;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
+use App\Http\Controllers\ChatController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -33,3 +33,20 @@ Route::middleware([
         return Inertia::render('Dashboard');
     })->name('dashboard');
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/chat', function () {
+        return Inertia::render('Chat/container');
+    })->name('chat');
+});
+
+
+Route::middleware('auth:sanctum')->get('/chat/rooms',[ChatController::class,'rooms']);
+Route::middleware('auth:sanctum')->get('/chat/room/{roomId}/messages',[ChatController::class,'messages']);
+Route::middleware('auth:sanctum')->post('/chat/room/{roomId}/message',[ChatController::class,'newMessage']);
+
+
