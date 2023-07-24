9d77bfd8daa22592760b8feb9d881e76d66fa210
abdurrahmanekecik <abdurrahmanekecik@gmail.com>
2023-07-24 11:20:42 +0300

    Pusher & EventSystem Added

--- resources/js/Pages/Chat/container.vue
+++ resources/js/Pages/Chat/container.vue
@@ -20,7 +20,29 @@ export default defineComponent({
             messages:[]
         }
     },
+    watch:{
+        currentRoom(val, oldVal){
+            if( oldVal.id ){
+                this.disconnect( oldVal );
+            }
+            this.connect();
+        }
+    },
     methods:{
+        connect(){
+            if( this.currentRoom.id){
+                let vm = this;
+                this.getMessages();
+                window.Echo.private("chat." + this.currentRoom.id)
+                .listen('.message.new', e => {
+                    vm.getMessages();
+                })
+
+            }
+        },
+        disconnect(room){
+            window.Echo.leave("chat." + room.id)
+        },
         getRooms(){
             axios.get('/chat/rooms')
                 .then(response => {
@@ -33,7 +55,6 @@ export default defineComponent({
         },
         setRoom( room ){
             this.currentRoom = room;
-            this.getMessages();
         },
         getMessages(){
             axios.get('/chat/room/' + this.currentRoom.id + '/messages')
